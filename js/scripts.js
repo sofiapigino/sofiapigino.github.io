9ed01d4f4234d163a0c3e1fe5688ec2288a71134
Sofia Pigino <53062786+sofiapigino@users.noreply.github.com>
2020-12-10 15:42:40 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
     */
-    (function ($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
             location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
+            this.pathname.replace(/^\//, "") &&
             location.hostname == this.hostname
         ) {
             var target = $(this.hash);
@@ -39,4 +39,19 @@
     $("body").scrollspy({
         target: "#sideNav",
     });
+
+    // Floating label headings for the contact form
+    $(function () {
+        $("body").on("input propertychange", ".floating-label-form-group", function (e) {
+            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function () {
+            $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function () {
+            $(this).removeClass("floating-label-form-group-with-focus");
+        });
+    });
+
+    //Tooltip
+    $('[data-toggle="tooltip"]').tooltip()
+
 })(jQuery); // End of use strict
